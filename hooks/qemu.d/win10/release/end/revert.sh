07ba5f0e43901f304e72747f3153b8f0ee22e705
David Flowers II <master4491@gmail.com>
2023-02-22 11:06:33 -0600

    Cleaned up

--- hooks/qemu.d/win10/release/end/revert.sh
+++ hooks/qemu.d/win10/release/end/revert.sh
@@ -1,36 +1,29 @@
 # debugging
 set -x
 
-# load vars
+# PCIe Variables
 source "/etc/libvirt/hooks/kvm.conf"
 
-# unload vfio
+# Unload VFIO
 modprobe -r vfio_pci
 modprobe -r vfio_iommu_type1
 modprobe -r vfio
 
-# bind gpu
+# Rebind the GPU
 virsh nodedev-reattach $VIRSH_GPU_VIDEO
 virsh nodedev-reattach $VIRSH_GPU_AUDIO
 
-# rebind VTconsoles
+# Rebind VTconsoles
 echo 1 > /sys/class/vtconsole/vtcon0/bind
 echo 0 > /sys/class/vtconsole/vtcon1/bind
 
-# bind efi framebuffer
-# echo "efi-framebuffer.0" > /sys/bus/platform/drivers/efi-framebuffer/bind
-
-#echo "pci_0000_28_00_0" > /sys/bus/pci/drivers/amdgpu/bind
-#echo "pci_0000_28_00_1" > /sys/bus/pci/drivers/snd_hda_intel/bind
-
-# load amd drivers
-#modprobe drm
+# Reload AMD and Intel Audio Drivers
 modprobe amdgpu
 modprobe snd_hda_intel
-#modprobe radeon
-#modprobe drm_kms_helper
 
 sleep 3
 
-# start gdm3
+# Start GDM3
 systemctl start gdm3.service
+
+# Login screen should pop-up shortly after the end of this
