7e55b99a46fcc25fe657a4f20af6b3ff02035561
David Flowers II <master4491@gmail.com>
2023-02-22 22:47:32 -0600

    Started adding libvirt and hooks to the Install script

--- test-Installer.sh
+++ test-Installer.sh
@@ -59,19 +59,21 @@ echo ""
 	echo "dpkg -i $1.deb"
 }
 
+: '
 echo "--------------------------------------------------"
 echo "      This script will install a lot of stuff.    "
-echo "  It may take a while depending on internet speed."
+echo " It may take a while depending on internet speed. "
 echo "      The script will start in 10 seconds.        "
 echo "                CTRL + C to Abort                 "
+echo "          THIS WILL REBOOT YOUR SYSTEM            "
 echo "--------------------------------------------------"
-#sleep 7s
-#echo "3"
-#sleep 1s
-#echo "2"
-#sleep 1s
-#echo "1"
-#sleep 1s
+sleep 7s
+echo "3"
+sleep 1s
+echo "2"
+sleep 1s
+echo "1"
+sleep 1s
 
 # apt 
 echo ""
@@ -82,18 +84,45 @@ echo ""
 sleep 1s
 echo "apt update"
 
+get-apt bridge-utils
 get-apt code "'deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main'"
 get-apt curl
 get-apt git
 get-apt gnome-tweaks
-get-apt libvirt-daemon
+get-apt qemu-kvm
+get-apt qemu-utils
+get-apt libvirt-daemon-system
+get-apt libvirt-clients
 get-apt openjdk-17-jdk
 get-apt openjdk-17-jre
+get-apt ovmf
 get-apt python3
 get-apt qemu-kvm
 get-apt steam
 get-apt virt-manager
 get-apt wine
+get-apt virt-manager
+'
 
-get-deb discord 'https://discord.com/api/download?platform=linux&format=deb'
-get-deb vivaldi 'https://downloads.vivaldi.com/stable/vivaldi-stable_5.6.2867.62-1_amd64.deb'
+#get-deb discord 'https://discord.com/api/download?platform=linux&format=deb'
+#get-deb vivaldi 'https://downloads.vivaldi.com/stable/vivaldi-stable_5.6.2867.62-1_amd64.deb'
+
+BOOT_CONFIG_FILE="/etc/default/grub"
+
+if ! grep -i -q "amd_iommu" "$BOOT_CONFIG_FILE"; then
+    $(sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="/&amd_iommu=on iommu=pt /' "$BOOT_CONFIG_FILE")
+else 
+    echo "AMD IOMMU All Good!"
+fi
+
+echo "grub-update"
+
+echo "cp -R ./hooks/ /etc/libvirt/"
+
+echo "usermod -a -G libvirt $(whoami)"
+
+echo "systemctl start libvirt"
+
+echo "systemctl enable libvirt"
+
+echo "reboot now"
